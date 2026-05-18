207fdff99c68d23ca691501bcac2ae1d3cfe409f
Estefany0921 <maria.piedrahita251@tau.usbmed.edu.co>
2026-05-18 11:03:16 -0500

    vercel3

--- backend/server.js
+++ backend/server.js
@@ -177,7 +177,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en http://localhost:${PORT}`);
 });
-/* ============================
+/* ==========================
    RUTAS DE USUARIOS
    ============================ */
 
