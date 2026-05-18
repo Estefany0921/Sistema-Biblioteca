ee78433156d41f395c34e5ff66c8c87998d5fc38
Estefany0921 <maria.piedrahita251@tau.usbmed.edu.co>
2026-05-18 12:17:37 -0500

    rutas-cors

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
+
 
 /* ============================
    CONEXIÓN A TURSO
